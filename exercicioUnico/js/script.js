24c02a1633fdd71eeb5b8039ed2f63fc0d0d633a
Gabriel da Silva <gabrieldasilvaprivado1@gmail.com>
2022-02-28 01:46:36 -0300

    Reorganizacao dos codigos, otimiza repeticao dos elementos

--- exercicioUnico/js/script.js
+++ exercicioUnico/js/script.js
@@ -1,63 +1,60 @@
 import * as elements from './elements.js';
-
-const calculateInvalidVotes = (result) => {
-  result =
-    Number(elements.votosBrancos.value) + Number(elements.votosNulos.value);
-  return result;
-};
-
-const calculateTotalVotes = (result) => {
-  result =
-    Number(elements.votosCandidatoA.value) +
-    Number(elements.votosCandidatoB.value) +
-    calculateInvalidVotes();
-  return result;
-};
-
-const percentCandidatoA = (percent) => {
-  percent = (
-    (Number(elements.votosCandidatoA.value) * 100) /
-    calculateTotalVotes()
-  ).toFixed(2);
-  return percent;
-};
-
-const percentCandidatoB = (percent) => {
-  percent = (
-    (Number(elements.votosCandidatoB.value) * 100) /
-    calculateTotalVotes()
-  ).toFixed(2);
-  return percent;
-};
-
-const percentInvalidVotes = (percent) => {
-  percent = ((calculateInvalidVotes() * 100) / calculateTotalVotes()).toFixed(
-    2
-  );
-  return percent;
-};
-const graphicInfo = () => {
-  elements.infoCandidatoA.innerHTML = `Candidato A: ${percentCandidatoA()}% (${Number(
-    elements.votosCandidatoA.value
-  )} votos)`;
-  elements.barraCandidatoA.style.width = `${percentCandidatoA()}%`;
-
-  elements.infoCandidatoB.innerHTML = `Candidato B: ${percentCandidatoB()}% (${Number(
-    elements.votosCandidatoB.value
-  )} votos)`;
-  elements.barraCandidatoB.style.width = `${percentCandidatoB()}%`;
-
-  elements.infoVotosInvalidos.innerHTML = `Inválidos: ${percentInvalidVotes()}% (${calculateInvalidVotes()} votos)`;
-  elements.barraVotosInvalidos.style.width = `${percentInvalidVotes()}%`;
-};
-
 const handleSubmit = (event) => {
   event.preventDefault();
 
-  elements.valueVotosA.innerHTML = ` ${elements.votosCandidatoA.value}`;
-  elements.valueVotosB.innerHTML = ` ${elements.votosCandidatoB.value}`;
+  const votosCandidatoA = Number(elements.votosCandidatoA.value);
+  const votosCandidatoB = Number(elements.votosCandidatoB.value);
+
+  const calculateInvalidVotes = (result) => {
+    result =
+      Number(elements.votosBrancos.value) + Number(elements.votosNulos.value);
+    return result;
+  };
+
+  const calculateTotalVotes = (result) => {
+    result = votosCandidatoA + votosCandidatoB + calculateInvalidVotes();
+    return result;
+  };
+
+  const percentCandidatoA = (percent) => {
+    percent = ((votosCandidatoA * 100) / calculateTotalVotes()).toFixed(2);
+    return percent;
+  };
+
+  const percentCandidatoB = (percent) => {
+    percent = ((votosCandidatoB * 100) / calculateTotalVotes()).toFixed(2);
+    return percent;
+  };
+
+  const percentInvalidVotes = (percent) => {
+    percent = ((calculateInvalidVotes() * 100) / calculateTotalVotes()).toFixed(
+      2
+    );
+    return percent;
+  };
+  const graphicInfo = () => {
+    const barraCandidatoA = elements.barraCandidatoA;
+    const barraCandidatoB = elements.barraCandidatoB;
+    const barraVotosInvalidos = elements.barraVotosInvalidos;
+
+    barraCandidatoA.style.width = '0px';
+    barraCandidatoB.style.width = '0px';
+    barraVotosInvalidos.style.width = '0px';
+    elements.infoCandidatoA.innerHTML = `Candidato A: ${percentCandidatoA()}% (${votosCandidatoA} votos)`;
+    barraCandidatoA.style.width = `${percentCandidatoA()}%`;
+
+    elements.infoCandidatoB.innerHTML = `Candidato B: ${percentCandidatoB()}% (${votosCandidatoB} votos)`;
+    barraCandidatoB.style.width = `${percentCandidatoB()}%`;
+
+    elements.infoVotosInvalidos.innerHTML = `Inválidos: ${percentInvalidVotes()}% (${calculateInvalidVotes()} votos)`;
+    barraVotosInvalidos.style.width = `${percentInvalidVotes()}%`;
+  };
+
+  elements.valueVotosA.innerHTML = ` ${votosCandidatoA}`;
+  elements.valueVotosB.innerHTML = ` ${votosCandidatoB}`;
   elements.valueVotosInvalidos.innerHTML = ` ${calculateInvalidVotes()}`;
   elements.totalVotos.innerHTML = ` ${calculateTotalVotes()}`;
+
   percentCandidatoA();
   percentCandidatoB();
   percentInvalidVotes();
