1404fb3ca9ff4c68d106233e2241761e3b356e31
Gabriel da Silva <gabrieldasilvaprivado1@gmail.com>
2022-04-09 21:30:13 -0300

    Finaliza segundo grafico da atividade de chartjs

--- PesquisaInovacao/chartjs/script.js
+++ PesquisaInovacao/chartjs/script.js
@@ -1,26 +1,45 @@
-const labels = ['12:00', '13:00', '14:00', '15:00', '16:00', '17:00'];
+const labelsTempUmidade1 = [
+  '12:00',
+  '13:00',
+  '14:00',
+  '15:00',
+  '16:00',
+  '17:00',
+];
+const labelsTempUmidade2 = [
+  'Janeiro',
+  'Fevereiro',
+  'Marco',
+  'Abril',
+  'Maio',
+  'Junho',
+];
 
-const data = {
-  labels: labels,
+const dataTempUmidade1 = {
+  labels: labelsTempUmidade1,
   datasets: [
     {
       label: 'Temperatura',
       backgroundColor: 'rgb(255, 99, 132)',
       borderColor: 'rgb(255, 99, 132)',
       data: [30, 29, 28, 25, 22, 23],
+      pointStyle: 'star',
+      pointRadius: 7,
     },
     {
       label: 'Umidade',
       backgroundColor: 'rgb(54, 162, 235)',
       borderColor: 'rgb(54, 162, 235)',
       data: [80, 82, 80, 85, 80, 83],
+      pointStyle: 'rectRot',
+      pointRadius: 7,
     },
   ],
 };
 
-const config = {
+const configTempUmidade1 = {
   type: 'line',
-  data: data,
+  data: dataTempUmidade1,
   options: {
     layout: {
       padding: 30,
@@ -28,6 +47,9 @@ const config = {
     plugins: {
       legend: {
         position: 'right',
+        labels: {
+          usePointStyle: true,
+        },
       },
       title: {
         display: true,
@@ -40,4 +62,58 @@ const config = {
   },
 };
 
-const myChart = new Chart(document.getElementById('chart'), config);
+const dataTempUmidade2 = {
+  labels: labelsTempUmidade2,
+  datasets: [
+    {
+      label: 'Temperatura Media',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: [22, 24, 27, 23, 20, 18],
+      pointStyle: 'rect',
+      pointRadius: 7,
+    },
+    {
+      label: 'Umidade Media',
+      backgroundColor: 'rgb(54, 162, 235)',
+      borderColor: 'rgb(54, 162, 235)',
+      data: [90, 89, 93, 87, 88, 82],
+      pointStyle: 'rect',
+      pointRadius: 7,
+    },
+  ],
+};
+
+const configTempUmidade2 = {
+  type: 'bar',
+  data: dataTempUmidade2,
+  options: {
+    layout: {
+      padding: 20,
+    },
+    plugins: {
+      legend: {
+        position: 'right',
+        labels: {
+          usePointStyle: true,
+        },
+      },
+      title: {
+        display: true,
+        text: 'Aula de ChartJS do Roverzão SPTech 2022',
+        font: {
+          size: 24,
+        },
+      },
+    },
+  },
+};
+
+const firstChart = new Chart(
+  document.getElementById('first_chart'),
+  configTempUmidade1
+);
+const secondChart = new Chart(
+  document.getElementById('second_chart'),
+  configTempUmidade2
+);
